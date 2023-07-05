5e91845b8294899e9947aa245f489dbf131cdcb1
weiowyw <haxjlebatop@gmail.com>
2023-07-05 18:39:36 +0500

    asdsda

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+
+// 1.
+// let a = 15, b = 27;
+//
+// const findMaxNumber = (first, second) => {
+//     let maxNumber;
+//     if(first > second) {
+//         maxNumber = first;
+//     }else maxNumber = second;
+//         return maxNumber;
+// };
+// const theMaxNumber = findMaxNumber(a,b);
+// console.log(theMaxNumber);
+
+
+// 2.
+// const age = prompt('Введите свой возраст', );
+//
+// const checkAge = (age) => age>=18;
+//
+// const result = checkAge(age);
+// console.log(result);
+
+
+// 3.
+// const userStrMaxLength = prompt('Введите максимальный размер строки', );
+// const maxLength = parseInt(userStrMaxLength);
+// const userStr = prompt('Введите строку', );
+//
+// const strSlice = (str, length) => {
+//     if (str.length > length) {
+//         str = str.slice(0, length);
+//         str = str + "...";
+//         return str;
+//     }else return str;
+// };
+// const cutString = strSlice(userStr, maxLength);
+// console.log(cutString);
+
+
+// 4.
+// let str = prompt('Введите строку', );
+// const word = "word";
+//
+// const checkWord = (str, word) => str.includes(word);
+// const result = checkWord(str, word);
+// console.log(result);
